6e2c48e011e25159a3de1221485530d82e6a568e
RoyOren <RoyOren91@gmail.com>
2017-01-25 11:08:42 +0200

    Added SendKudoActivity

--- app/src/main/java/com/example/or/kudos/SendKudoActivity.java
+++ app/src/main/java/com/example/or/kudos/SendKudoActivity.java
@@ -1,7 +1,15 @@
 package com.example.or.kudos;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SendKudoActivity extends AppCompatActivity {
 
@@ -9,5 +17,65 @@ public class SendKudoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_kudo);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(SendKudoActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(2, 2, 2, 2);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.cap, R.drawable.scarf,
+                R.drawable.shirt2, R.drawable.hoodie,
+                R.drawable.jacket, R.drawable.mitten,
+                R.drawable.shorts1, R.drawable.skirt1,
+                R.drawable.skirt2, R.drawable.trousers,
+                R.drawable.underwear1, R.drawable.socks,
+                R.drawable.sneaker
+        };
     }
 }
+
